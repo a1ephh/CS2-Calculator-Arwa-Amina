7f0c389f34df60b47670d0ac254073b113e02aaf
arwa <arwahasan@aucegypt.edu>
2025-04-03 21:00:10 +0200

    inor function declaration modifications in calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,5 @@
 #include "calculator.h"
 
-
-
 double addition(double x, double y) {
     return x + y;
 }
@@ -14,11 +12,14 @@ double multiplication(double x, double y) {
     return x * y;
 }
 
-double Division(double x, double y) {
-    return x / y;
+double division(double x, double y) {
+    if(y==0){return 0;} // give an error
+    return x / y; 
 }
 
-double Factorial(double x) {
+double factorial(double x) { 
+    if(x<0){return -1;} // give an error
+
     if (x == 0 || x == 1) {
         return 1;
     }
@@ -36,17 +37,16 @@ double GCD(double x, double y) {
     if (y == 0) {
         return x;
     }
-    return GCD(y, fmod(x, y));
+    return GCD(y, x%y);
 }
 
 double LCM(double x, double y) {
     return (x / GCD(x, y)) * y;
 }
 
-double Random(double x, double y) {
-    if (x > y) swap(x, y);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> dist(x, y);
-    return dist(gen);
-}
+double Random(T x, T y){ // range is x-y 
+    if(x>y) swap(x,y);
+    srand (time(NULL));
+    T number= rand() % y +x;
+    return number;
+} 
