95fb5cbb6ae383104361353a43b3b58dce3f50c3
aminaelshafei <aminaelshafei@aucegypt.edu>
2025-04-03 20:56:21 +0200

    testing calculator

--- test.cpp
+++ test.cpp
@@ -12,8 +12,28 @@ Greatest Common Divisor (GCD) calculation
 Least Common Multiple (LCM) calculation
 Random number generation (within a range) */
 #include <iostream>
+#include "calculator.h"
+
 using namespace std;
 
-int main(){
-    
+int main() {
+    double a = 22.0, b = 3.5;
+
+    cout << "Addition of a and b is: " << addition(a, b) << endl;
+    cout << "Subtraction of a and b is: " << subtraction(a, b) << endl;
+    cout << "Multiplication of a and b is: " << multiplication(a, b) << endl;
+    cout << "Division of a and b is: " << Division(a, b) << endl;
+    cout << "Factorial of a is: " << Factorial(a) << endl;
+    cout << "Factorial of b is: " << Factorial(b) << endl;
+    cout << "GCD of a and b is: " << GCD(a, b) << endl;
+    cout << "LCM of a and b is: " << LCM(a, b) << endl;
+
+    double rand1 = Random(1.0, 100.0);
+    double rand2 = Random(1.0, 100.0);
+    cout << "Random number 1 is: " << rand1 << endl;
+    cout << "Random number 2 is: " << rand2 << endl;
+
+    cout << "Before swap: a = " << a << ", b = " << b << endl;
+    swap(a, b);
+    cout << "After swap: a = " << a << ", b = " << b << endl;
 }
